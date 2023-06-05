91b22508e05ca0603cad1b8427aabf43bb45b4a0
Jundy25 <127575259+Jundy25@users.noreply.github.com>
2023-06-05 11:32:25 +0800

    Update edit.php

--- IM 6-04-2023/edit.php
+++ IM 6-04-2023/edit.php
@@ -11,20 +11,25 @@
 			$id=$_GET['id'];
 			$query=mysqli_query($conn,"SELECT id, item_name, quantity, price, quantity*price AS total, DATE(created_at) AS date FROM uthangs JOIN items ON uthangs.item_id=items.item_id WHERE id='$id';");
 			$row=mysqli_fetch_array($query);
+			
 		?>
+			  <?php 
+			  $bname = $REQUEST['bname'];
+			  
+				
 
    <tr><td>
 	  <label for="item_name">Item</label></td><td>
-	  <input type="text" name="item" id="item" value=<?php echo $row["item"];  ?> >
+	  <input type="text" name="item" id="item" value=<?php echo $REQUEST["item_name"];?> >
 	  </td></tr>      
 	
    <tr><td>
 	  <label for="quantity">Quantity:</label></td><td>
-	  <input type="text" name="quant" id="quant" value='<?php  echo $row["quantity"];  ?>'>
+	  <input type="text" name="quant" id="quant" value='<?php  echo $REQUEST["quantity"];  ?>'>
 	  </td></tr>
 
 	  <tr><td>
-		 <input type="hidden" name="cre" id="cre" value ='<?php echo $row["credate"];  ?>'>
+		 <input type="hidden" name="cre" id="cre" value ='<?php echo $REQUEST["credate"];  ?>'>
    <input type="submit" value="Submit">
    </td></tr>
 	  </div>
